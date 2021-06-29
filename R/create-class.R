b1f788a1a5c2a572a26392beb8e3d9f40b92113c
Brandyn Lucca <brandyn.lucca@gmail.com>
2021-06-29 10:39:10 -0400

    Fixed some major issues with installation.

---
+++ R/create-class.R
@@ -0,0 +1,148 @@
+#' Generate ISO-class object containing isotope data, metadata, and other separators.
+#'
+#' @param x text
+#'
+#' @usage
+#' ISOgen(x)
+#'
+#' @examples
+#' text
+#'
+#' @return
+#' Generates ISO-class object.
+#'
+#' @export
+#' @import tidyverse
+
+ISOgen <- function(data,
+                   data.names = NULL,
+                   seperator = NA,
+                   doi, dfi, lambda) {
+    requireNamespace("tidyverse", quietly = T)
+    # Parse different isotope data; separate into lists for appending
+    if (is_tibble(data)) {
+        isodata <- data %>%
+            group_splitn(data.names)
+    } else if (is_list(data)) {
+        var <- names(data)
+        data <- as.data.frame(data)
+        colnames(data) <- c("value", "residence")
+        isodata <- data %>%
+            lst() %>%
+            rlang::set_names(var)
+    } else if (class(data) == "numeric") {
+        isodata <- data %>%
+            enframe(., name = NULL) %>%
+            lst() %>%
+            rlang::set_names(data.names)
+    }
+
+    # Generate metadata
+    metadata <- data.frame(doi = doi,
+                           dfi = dfi,
+                           lambda = lambda)
+
+    return(new("ISO",
+               metadata = metadata,
+               data = isodata,
+               timestamp = Sys.time()))
+}
+
+#' Generate MISO-class object.
+#'
+#' @param x text
+#'
+#' @usage
+#' MISOgen(x)
+#'
+#' @examples
+#' text
+#'
+#' @return
+#' Generates MISO-class object.
+#'
+#' @export
+#' @import tidyverse
+
+MISOgen <- function(ISOobjects) {
+    requireNamespace("tidyverse", quietly = T)
+
+    # Wrap ISO objects for MISO
+    wrapped <- c(ISOobjects) %>%
+        tibble()
+
+    ma <- list()
+    da <- list()
+
+    for (i in seq_len(nrow(wrapped))) {
+        md <- wrapped %>%
+            slice(i) %>%
+            unlist()
+        m <- md[[1]]@metadata
+        d <- md[[1]]@data
+        ma[[i]] <- m
+        da[[i]] <- d
+    }
+
+    return(new("MISO",
+               metadata = ma,
+               data = da,
+               timestamp = Sys.time()))
+}
+
+#' Parameter pull function.
+#'
+#' @description
+#' Function for pulling parameters
+#' @export
+setGeneric("get_param", function(object) standardGeneric("get_param"))
+setMethod("get_param", signature(object="ISO"),
+          function(object){
+            params <- new("param",
+                          parameter=names(object@data),
+                          dt=object@data[[sym(names(object@data))]]$value,
+                          doi=object@metadata$doi, dfi=object@metadata$dfi,
+                          lambda=object@metadata$lambda
+            )
+            params_df <- as.data.frame(
+              setNames(
+                lapply(slotNames(params),
+                       function(x){
+                         slot(params, x)
+                       }),
+                slotNames(params))
+            )
+
+            return(params_df)
+            })
+
+#' Value pull
+#'
+#' @description
+#' Function for pulling data values
+#' @export
+setGeneric("get_param_sub",
+           function(object, index) standardGeneric("get_param_sub"))
+setMethod("get_param_sub",
+          signature(object="MISO"),
+          function(object, index){
+            newobj <- ISOgen(data=object@data[[index]][[sym(names(object@data[[index]]))]]$value,
+                             data.names=names(object@data[[index]]),
+                             doi=object@metadata[[index]]$doi,
+                             dfi=object@metadata[[index]]$dfi,
+                             lambda=object@metadata[[index]]$lambda)
+            param_sub <- get_param(newobj)
+
+            return(param_sub)
+          })
+
+#' Pull data length
+#'
+#' @description
+#' Function for getting MISO length
+#' @export
+setMethod("length", "MISO",
+          function(x){
+            length(x@data)
+          })
+
---
+++ R/utilities.R
@@ -0,0 +1,162 @@
+#' Method for what is printed for ISO objects.
+#' @export
+setMethod("show", "ISO", function(object) {
+    cat(paste(is(object)[[1]], " object", sep = ""), "\n", "Samples: ", max(unlist(lapply(object@data, dim))), "\n", "Isotopes: ", unique(names(object@data)),
+        "\n", "Origin steady state isotopic composition (d0): ", median(object@metadata$doi), "\n", "New steady state isotopic composition (df):",
+        median(object@metadata$dfi), "\n", "Tissue-specific isotopic turnover rate (lambda):", median(object@metadata$lambda), "\n", "Generated: ",
+        paste(object@timestamp))
+})
+
+#' Method for what is printed for MISO objects.
+#' @export
+setMethod("show", "MISO", function(object) {
+    cat(paste(is(object)[[1]], " object", sep = ""), "\n", "Number of animals: ", length(object@data), "\n", "Reference sample count: ", max(unlist(lapply(object@data[[1]],
+        dim))), "\n", "Isotopes: ", unique(names(object@data[[1]])), "\n", "Ref origin steady state isotopic composition (d0): ", median(object@metadata[[1]]$doi),
+        "\n", "Ref new steady state isotopic composition (df):", median(object@metadata[[1]]$dfi), "\n", "Ref tissue-specific isotopic turnover rate (lambda):",
+        median(object@metadata[[1]]$lambda), "\n", "Generated: ", paste(object@timestamp))
+})
+
+#' Method for what is printed for ISO object summaries.
+#' @export
+setMethod("summary", "ISO", function(object) {
+    cat(paste(class(object)[[1]], " object", sep = ""), "\n", "Number of samples: ", max(unlist(lapply(object@data, dim))), "\n", "Isotopes measured: ",
+        unique(names(object@data)), "\n", "Median origin steady state isotopic composition (d0): ", median(object@metadata$doi), "\n", "Median new location steady state isotopic composition (df): ",
+        median(object@metadata$dfi), "\n", "Median tissue-specific isotopic turnover rate (lambda): ", median(object@metadata$lambda), "\n", "Median residence time (days): ",
+        pull(object@data[[sym(unique(names(object@data)))]], residence) %>% tibble %>% summarise(val = quantile(., probs = 0.5, names = F, na.rm = T)) %>%
+            .$val, " [", pull(object@data[[sym(unique(names(object@data)))]], residence) %>% tibble %>% summarise(val = quantile(., probs = 0.025,
+            names = F, na.rm = T)) %>% .$val, ", ", pull(object@data[[sym(unique(names(object@data)))]], residence) %>% tibble %>% summarise(val = quantile(.,
+            probs = 0.975, names = F, na.rm = T)) %>% .$val, ", 95% CI]", "\n", "Median residence time (days) -- trimmed [0, Inf]: ", pull(object@data[[sym(unique(names(object@data)))]],
+            residence) %>% tibble %>% filter(. > 0) %>% na.omit() %>% summarise(val = quantile(., probs = 0.5, names = F)) %>% .$val, " [", pull(object@data[[sym(unique(names(object@data)))]],
+            residence) %>% tibble %>% filter(. > 0) %>% na.omit() %>% summarise(val = quantile(., probs = 0.025, names = F)) %>% .$val, ", ", pull(object@data[[sym(unique(names(object@data)))]],
+            residence) %>% tibble %>% filter(. > 0) %>% na.omit() %>% summarise(val = quantile(., probs = 0.975, names = F)) %>% .$val, ", 95% CI]",
+        sep = "")
+})
+
+#' Method for what is printed for MISO object summaries.
+#' @export
+setMethod("summary", "ISO", function(object) {
+  cat(paste(class(object)[[1]], " object", sep=""),
+      "\n", "Number of ISO objects: ", (length(object)), "\n",
+      "Isotopes measured: ", unique(names(object@data[[1]])), "\n",
+      paste("Mean ", unique(names(object@data[[1]])),": ", sep=""))
+})
+
+
+##' Edit ISO objects.
+#' @export
+
+ISOedit <- function(object, ...){
+  params <- get_param(object)
+  res <- REScalc(object)
+  object@data[[sym(params$parameter[1])]]$residence <- res
+  return(object)
+}
+
+#' @rdname ISOedit
+MISOedit <- function(object, rejection = F){
+  nn <- seq_len(length(object@data))
+
+  for(i in nn){
+    if(rejection == F){
+      params <- get_param_sub(object, i)
+      res <- REScalc(object, i)
+      object@data[[i]][[sym(params$parameter[1])]]$residence <- res
+    }else{
+      object <- rejection_sample(object, i)
+    }
+  }
+  return(object)
+}
+
+#' Generates a named list.
+#'
+#' @param .tbl text
+#' @usage
+#' text
+#' @examples
+#' test
+#' @return
+#' text
+#' @export
+
+group_splitn <- function(.tbl, data.names) {
+    select(.tbl, data.names) %>%
+    gather(key = "Isotope", value = "value") %>%
+    group_split(Isotope, .keep = F) %>%
+    rlang::set_names(unique(data.names))
+}
+
+#' Class filter list
+#'
+#' @param object text. Checks global environment for current ISO/MISO object names.
+#' @usage
+#' text
+#' @examples
+#' test
+#' @return
+#' text
+#' @export
+
+ISOfind <- function(object) {
+    inherits(get(object), "ISO")
+}
+
+#' Add metadata
+#'
+#' @param ISOobject text. Edits this object.
+#' @usage
+#' text
+#' @examples
+#' test
+#' @return
+#' text
+#' @export
+ISOadd <- function(object, update, data.names=NULL){
+  to_add <- data.frame(update)
+
+  if(!is.null(data.names)){
+    colnames(to_add) <- data.names
+    to_add_sub <- to_add %>%
+      select(all_of(data.names))
+  }else{
+    to_add_sub <- to_add
+  }
+  vars <- colnames(to_add_sub)
+  tmpobj <- object
+
+  for(i in seq_len(length(vars))){
+    tmpobj@metadata[[sym(vars[i])]] <- to_add_sub[[sym(vars[i])]]
+  }
+
+  return(tmpobj)
+}
+
+#' Pull values
+#'
+#' @param ISOobject text. Pull values
+#' @usage
+#' text
+#' @examples
+#' test
+#' @return
+#' text
+#' @export
+ISOpull <- function(object, data.names=NULL){
+  if(!is.null(data.names)){
+    return(object@data[[sym(data.names)]])
+  }
+  else{
+    return(object@data)
+  }
+}
+
+#' @rdname ISOpull
+#' @export
+MISOpull <- function(object, index, data.names=NULL){
+  if(!is.null(data.names)){
+    return(object@data[[index]][[sym(data.names)]])
+  }
+  else{
+    return(object@data[[index]][[1]])
+  }
+}
